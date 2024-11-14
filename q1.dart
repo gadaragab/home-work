b1e5fc273196f7e5661ec079b48cb69c32fc609d
Ghada Ragab <ragabg048@gmail.com>
2024-11-14 22:09:20 +0200

    Add files via upload

--- q1.dart
+++ q1.dart
@@ -1,8 +1,12 @@
-/*Create a string variable greeting with the value 'Hello, Dart!'.
-- Print the length of greeting.
-- Convert greeting to uppercase and print it */
+/*Exercise: Write a Dart program with a main function that prints 'Hello, Dart World!' to the console. Modify it to print 'Hello,
+[Your Name]!' by using a variable.*/
+import 'dart:io';
+
 void main() {
-  String greeting = 'Hello, Dart!';
-  print(greeting.length);
-  print(greeting.toUpperCase());
+  print('Hello, Dart World!');
+
+  print('enter your name');
+  String? name = stdin.readLineSync()!;
+
+  print('Hello,[$name]!');
 }
--- q2.dart
+++ q2.dart
@@ -1,10 +1,14 @@
-/*- Create a list named colors with the values ['red', 'green', 'blue'].
-- Add 'yellow' to the list and print the updated list.
-- Remove 'green' from the list and print the updated list*/
+/*Create a Dart program that declares and initializes variables of different types (int, double, String, bool).
+Calculate the sum of two numbers, concatenate two strings, and print out the results.*/
 void main() {
-  List<String> colors = ['red', 'green', 'blue'];
-  colors.add('yellow');
-  print(colors);
-  colors.remove('green');
-  print(colors);
+  int number = 14;
+  double number2 = 13.5;
+  int number3 = 17;
+  String name = 'hello dart';
+  String name2 = 'ghada,';
+  bool isHungry = false;
+
+  int sum = number + number3;
+  print('the sum of two numbers is: $sum');
+  print(name2 + name);
 }
