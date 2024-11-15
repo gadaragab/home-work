08235cdac3fcecf58679f0d656f0b306d9633d8d
Ghada Ragab <ragabg048@gmail.com>
2024-11-15 09:14:10 +0200

    Update q7.dart

--- q7.dart
+++ q7.dart
@@ -1,3 +1,6 @@
+/*Exercise: Create a function called calculateGrade that takes a score (0 to 100) as a parameter and returns a letter grade
+(e.g., A, B, C). Then write a main function to call calculateGrade and print the grade for a given score. */
+
 void main() {
   calculateGrade(70);
 }
